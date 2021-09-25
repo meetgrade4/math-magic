95b9be8fe46feefe1bcb93828589109f9edb2db6
meetgrade4 <80895724+meetgrade4@users.noreply.github.com>
2021-09-25 08:50:14 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,7 @@
+function send() {
+    number_1 = document.getElementById("number1").value;
+    number_2 = document.getElementById("number2").value;
+    correct = parseInt(number_1) * parseInt(number_2);
+    tag1 = "<h4>" + number_1 + "X" + number_2 + "</h4>";
+    tag2 = "<br>Ansewer: <input type='text' placeholder='ansewer' id='ansewer></br>"
+}
